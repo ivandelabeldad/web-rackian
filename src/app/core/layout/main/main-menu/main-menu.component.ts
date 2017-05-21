a250193b95990b45da5b0ffe1e5e358a9c0c4e90
Ivan de la Beldad <ivandelabeldad@gmail.com>
2017-05-21 18:28:19 +0100

    Allow file uploads.

--- src/app/core/layout/main/main-menu/main-menu.component.ts
+++ src/app/core/layout/main/main-menu/main-menu.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../../../shared/authentication/user';
+import { RequestOptions, Headers } from '@angular/http';
+import { AuthHttpService } from '../../../../shared/authentication/auth-http.service';
+import { Observable } from 'rxjs/Observable';
+import { conf } from '../../../../conf';
 
 @Component({
   selector: 'rackian-main-menu',
@@ -10,10 +14,33 @@ export class MainBarComponent implements OnInit {
 
   user: User;
 
-  constructor(private userService: User) { }
+  constructor(private userService: User, private http: AuthHttpService) {
+  }
 
   ngOnInit() {
     this.user = this.userService;
+  }
+
+  fileUpload(evt) {
+    const fileList: FileList = evt.target.files;
+    if (fileList.length <= 0) {
+      return;
+    }
+
+    const file: File = fileList[0];
+    const formData: FormData = new FormData();
+    formData.append('link', file);
+    formData.append('link', 'value of the property');
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    const options = new RequestOptions({ headers });
+    this.http.post(`${conf.url.api.files}`, formData, options)
+      .map(res => res.json())
+      .catch(error => Observable.throw(error))
+      .subscribe(
+        data => console.log('success'),
+        error => console.log(error)
+      );
   }
 
 }
